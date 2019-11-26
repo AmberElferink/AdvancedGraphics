484ac8f972a08e1692d654f6acc03f1ad8789e97
Ymkeli <ymkewe@gmail.com>
2019-11-26 16:13:03 +0100

    turned off all the lights
    Former-commit-id: bf08ef57fa9e14a421be7b2aaca8cf03d9335eda

--- apps/imguiapp/main.cpp
+++ apps/imguiapp/main.cpp
@@ -47,19 +47,19 @@ void PrepareScene()
 	renderer->SetNodeTransform(rootNode, mat4::RotateX(-PI / 2));
 #if 1
 	// overhead light, use regular PT
-	int lightMat = renderer->AddMaterial(make_float3(100, 100, 80));
-	int lightQuad = renderer->AddQuad(make_float3(0, -1, 0), make_float3(0, 26.0f, 0), 6.9f, 6.9f, lightMat);
+	//int lightMat = renderer->AddMaterial(make_float3(100, 100, 80));
+	//int lightQuad = renderer->AddQuad(make_float3(0, -1, 0), make_float3(0, 26.0f, 0), 6.9f, 6.9f, lightMat);
 #else
 	// difficult light; use BDPT
-	int lightMat = renderer->AddMaterial(make_float3(500, 500, 400));
-	int lightQuad = renderer->AddQuad(make_float3(0.15188693, -0.32204545, 0.93446094), make_float3(-12.938412, -5.0068984, -25.725601), 1.9f, 1.9f, lightMat);
+	//int lightMat = renderer->AddMaterial(make_float3(500, 500, 400));
+	//int lightQuad = renderer->AddQuad(make_float3(0.15188693, -0.32204545, 0.93446094), make_float3(-12.938412, -5.0068984, -25.725601), 1.9f, 1.9f, lightMat);
 #endif
-	int lightInst = renderer->AddInstance(lightQuad);
+	//int lightInst = renderer->AddInstance(lightQuad);
 	// optional animated models
 	// renderer->AddScene( "CesiumMan.glb", "data/", mat4::Translate( 0, -2, -9 ) );
 	// renderer->AddScene( "project_polly.glb", "data/", mat4::Translate( 4.5f, -5.45f, -5.2f ) * mat4::Scale( 2 ) );
 	// load changed materials
-	renderer->DeserializeMaterials(materialFile.c_str());
+	//renderer->DeserializeMaterials(materialFile.c_str());
 }
 
 //  +-----------------------------------------------------------------------------+
